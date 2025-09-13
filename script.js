9725c59368896d7528f317b5d20d2c5b481d1290
Shivaprasad-hash <shivaprasad282001@gmail.com>
2025-09-13 22:03:07 +0530

    updated project

--- script.js
+++ script.js
@@ -11,4 +11,30 @@ crossIcon.addEventListener('click',function(){
     menuItems.classList.add("d-none");
     crossIcon.classList.add("d-none");
     hambergerIcon.classList.remove("d-none");
-})
+});
+
+let homeBtnSmall = document.getElementById("homeBtnSmall");
+
+homeBtnSmall.onclick = () => {
+    menuItems.classList.toggle("d-none");
+    crossIcon.classList.add("d-none");
+    hambergerIcon.classList.remove("d-none");
+}
+let aboutBtnSmall = document.getElementById("aboutBtnSmall");
+let aboutSection = document.getElementById("aboutSection")
+
+aboutBtnSmall.onclick = () => {
+    menuItems.classList.toggle("d-none");
+    crossIcon.classList.add("d-none");
+    hambergerIcon.classList.remove("d-none");
+    aboutSection.style.scrollMarginTop = "80px";
+}
+let skillsBtnSmall = document.getElementById("skillsBtnSmall");
+let skillsSection = document.getElementById("skillsSection");
+
+skillsBtnSmall.onclick = () => {
+    menuItems.classList.toggle("d-none");
+    crossIcon.classList.add("d-none");
+    hambergerIcon.classList.remove("d-none");
+    skillsSection.style.scrollMarginTop = "80px";
+}
