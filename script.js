d6a3796e20f579953b4c119cba6bec200cadb3b4
Shivaprasad-hash <shivaprasad282001@gmail.com>
2025-09-18 23:12:46 +0530

    updated project

--- script.js
+++ script.js
@@ -39,8 +39,42 @@ skillsBtnSmall.onclick = () => {
     skillsSection.style.scrollMarginTop = "80px";
 }
 
-let aboutmeBtn = document.getElementById("aboutmeBtn");
+let projSection = document.getElementById("projSection");
+let projectsBtnSmall = document.getElementById("projectsBtnSmall");
 
-aboutmeBtn.onclick = () => {
-    aboutSection.style.scrollMarginTop = "80px";
+projectsBtnSmall.onclick = () => {
+    menuItems.classList.toggle("d-none");
+    crossIcon.classList.add("d-none");
+    hambergerIcon.classList.remove("d-none");
+    projSection.style.scrollMarginTop = "80px";
+}
+
+let eduSection = document.getElementById("eduSection");
+let eduBtnSmall = document.getElementById("eduBtnSmall");
+
+eduBtnSmall.onclick = () => {
+    menuItems.classList.toggle("d-none");
+    crossIcon.classList.add("d-none");
+    hambergerIcon.classList.remove("d-none");
+    eduSection.style.scrollMarginTop = "80px";
+}
+
+let certifiSection = document.getElementById("certifiSection");
+let certifiBtnSmall = document.getElementById("certifiBtnSmall");
+
+certifiBtnSmall.onclick = () => {
+    menuItems.classList.toggle("d-none");
+    crossIcon.classList.add("d-none");
+    hambergerIcon.classList.remove("d-none");
+    certifiSection.style.scrollMarginTop = "80px";
+}
+
+let contactSection = document.getElementById("contactSection");
+let contBtnSmall = document.getElementById("contBtnSmall");
+
+contBtnSmall.onclick = () => {
+    menuItems.classList.toggle("d-none");
+    crossIcon.classList.add("d-none");
+    hambergerIcon.classList.remove("d-none");
+    contactSection.style.scrollMarginTop = "80px";
 }
